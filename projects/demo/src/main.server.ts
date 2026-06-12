26cf6d0749e0d460779bf5414b44e66b563620ae
Bobby Quantum <bobby@quantum.observer>
2026-06-12 21:26:07 +0100

    chore: upgrade to Angular 22 (#1)
    * chore: upgrade to Angular 22
    
    - Bump @angular/*, @angular-devkit/build-angular, @angular/cli,
      @angular/compiler-cli, ng-packagr, angular-eslint to ^22.0.0
    - Migrate builders from @angular-devkit/build-angular to @angular/build
    - Bump typescript to ~6.0.0 and typescript-eslint to ^8.59.0
    - Fix new lint rules: prefer-inject, prefer-control-flow, no-implicit-coercion
    - Update SSR: BootstrapContext arg, CommonEngine allowedHosts, ssr build option
    - Bump library version/peer deps to 22
    
    * fix: point demo badges to @bobbyquantum/ngx-editor

--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,7 +1,10 @@
+import { PlatformRef } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = ({ platformRef }: { platformRef: PlatformRef }) => {
+  return bootstrapApplication(AppComponent, config, { platformRef });
+};
 
 export default bootstrap;
